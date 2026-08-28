7baea5f4c77882d7c266e8bb3f6af16807871852
COSME CARVALHO DA SILVA NETO <cosmecarvalhog@gmail.com>
2026-08-28 19:37:12 -0300

    V5 = Refactor movement logic for square element

--- movimentaçãoQuadrado.js
+++ movimentaçãoQuadrado.js
@@ -2,26 +2,26 @@ const quadrado = document.querySelector(".quadrado")
 
 import { pontoTF, pontoTI } from "./translação.js";
 
-let noPontoF = false;
+let noPontoTF = false;
 
 document.addEventListener('keydown', function(event) {
 
     //ComeçoMovimentaçãoTranslação
     if (event.key == '1') {
 
-        if(noPontoF == false) {
+        if(noPontoTF == false) {
            
             quadrado.style.left = pontoTF[0][0] + "px";
             quadrado.style.top = pontoTF[1][0] + "px";
 
-        noPontoF = true;
+        noPontoTF = true;
 
         }else{
 
             quadrado.style.left = pontoTI[0][0] + "px";
             quadrado.style.top = pontoTI[1][0] + "px";
 
-        noPontoF = false;
+        noPontoTF = false;
         
         }}
     //TerminoMovimentaçãoTranslação

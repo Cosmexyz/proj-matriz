ab2445f83fc14586d62a7e8169886a356645bd40
COSME CARVALHO DA SILVA NETO <cosmecarvalhog@gmail.com>
2026-08-28 18:55:03 -0300

    V3 = Refactor translation logic and variable names
    Updated variable names for clarity and commented out unused code.

--- translação.js
+++ translação.js
@@ -1,34 +1,34 @@
 const quadrado = document.querySelector(".quadrado")
 
-const posição = quadrado.getBoundingClientRect()
+//const posição = quadrado.getBoundingClientRect()
 
 const matriz = [
     [1, 0, 100],
     [0, 1, 50],
     [0, 0, 1],
 ] 
-
-const pontoI = [
+// TI = transação Inicial
+const pontoTI = [
     [posição.x ],
     [posição.y],
     [1]
 ];
 
-function multiplicar(matriz, pontoI) {
+function multiplicar(matriz, pontoTI) {
     const x =
-    matriz[0][0] * pontoI[0][0] +
-    matriz[0][1] * pontoI[1][0] +
-    matriz[0][2] * pontoI[2][0];
+    matriz[0][0] * pontoTI[0][0] +
+    matriz[0][1] * pontoTI[1][0] +
+    matriz[0][2] * pontoTI[2][0];
 
     const y =
-     matriz[1][0] * pontoI[0][0]
-   + matriz[1][1] * pontoI[1][0]
-   + matriz[1][2] * pontoI[2][0];
+     matriz[1][0] * pontoTI[0][0]
+   + matriz[1][1] * pontoTI[1][0]
+   + matriz[1][2] * pontoTI[2][0];
 
    const h =
-     matriz[2][0] * pontoI[0][0]
-   + matriz[2][1] * pontoI[1][0]
-   + matriz[2][2] * pontoI[2][0];
+     matriz[2][0] * pontoTI[0][0]
+   + matriz[2][1] * pontoTI[1][0]
+   + matriz[2][2] * pontoTI[2][0];
 
    return[
     [x],
@@ -38,8 +38,9 @@ function multiplicar(matriz, pontoI) {
 }
 
 
-const resultado = multiplicar(matriz, pontoI)
+const resultado = multiplicar(matriz, pontoTI)
 
-const pontoF = resultado
+// TF = translação Final
+const pontoTF = resultado
 
-export{pontoF, pontoI};
+export{pontoTF, pontoTI};
